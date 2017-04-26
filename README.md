1df8909d8171373136b294a1b8247b2865af79d8
Vincent Chang <vincent.chang@morningstar.com>
2017-04-26 15:42:55 +0800

    add server.js and some commnet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # miniServer
 build up a http server on local by node
+
+1. copy server.js on the location of your project
+2. run 'node server'.
+3. open http://localhost:3000/test.html on your browser.
---
+++ server.js
@@ -0,0 +1,67 @@
+var PORT = 3000;
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var mine = {
+    "css": "text/css",
+    "gif": "image/gif",
+    "html": "text/html",
+    "ico": "image/x-icon",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "swf": "application/x-shockwave-flash",
+    "tiff": "image/tiff",
+    "txt": "text/plain",
+    "wav": "audio/x-wav",
+    "wma": "audio/x-ms-wma",
+    "wmv": "video/x-ms-wmv",
+    "xml": "text/xml"
+};
+
+
+var server = http.createServer(function(request, response) {
+    if (request.url === '/favicon.ico') {
+        response.end();
+    } else {
+        var pathname = url.parse(request.url).pathname;
+        var realPath = path.join(__dirname, pathname);
+
+        var ext = path.extname(realPath);
+        ext = ext ? ext.slice(1) : 'unknown';
+        fs.exists(realPath, function(exists) {
+            if (!exists) {
+                response.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                response.write("This request URL " + pathname + " was not found on this server.");
+                response.end();
+            } else {
+                fs.readFile(realPath, "binary", function(err, file) {
+                    if (err) {
+                        response.writeHead(500, {
+                            'Content-Type': 'text/plain'
+                        });
+                        response.end(err);
+                    } else {
+                        var contentType = mine[ext] || "text/plain";
+                        response.writeHead(200, {
+                            'Content-Type': contentType
+                        });
+                        response.write(file, "binary");
+                        response.end();
+                    }
+                });
+            }
+        });
+    }
+});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
